b51d2cec50f0753586127ebcf00bb3d183226914
Ramas <web.sprendimai4@gmail.com>
2021-08-02 12:44:35 +0300

    Added first test.

--- tests/Unit/VendingMachineTest.php
+++ tests/Unit/VendingMachineTest.php
@@ -6,5 +6,13 @@ use PHPUnit\Framework\TestCase;
 
 class VendingMachineTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        $this->vendingMachine = new VendingMachine();
+    }
 
+    public function test_should_initialize_vending_machine_class()
+    {
+        $this->assertInstanceOf(VendingMachine::class, $this->vendingMachine);
+    }
 }
