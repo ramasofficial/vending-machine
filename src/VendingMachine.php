b9c3ff043c17c71310e4c30173e9eb1a950472bf
Ramas <web.sprendimai4@gmail.com>
2021-08-02 12:57:32 +0300

    Passed 1 coin test.

--- src/VendingMachine.php
+++ src/VendingMachine.php
@@ -6,5 +6,20 @@ namespace TDD;
 
 class VendingMachine
 {
-    
+    private $balance;
+
+    public function add($coin): bool
+    {
+        if($coin === 1) {
+            $this->balance += 1;
+            return true;
+        }
+
+        return false;
+    }
+
+    public function checkBalance()
+    {
+        return $this->balance;
+    }
 }
